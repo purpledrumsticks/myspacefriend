856e8e3d167c618970302892166c01a9861b3d9c
Inigo Montoya <inigo@MacBook-Pro.local>
2018-07-29 23:46:37 -0400

    put in swipe functionality??

--- js/app.js
+++ js/app.js
@@ -106,6 +106,36 @@
 
     previousArrow.onclick = handlePreviousArrow;
     nextArrow.onclick = handleNextArrow;
+
+    let touchstartX = 0;
+    let touchstartY = 0;
+    let touchendX = 0;
+    let touchendY = 0;
+
+    const gestureZone = photoDiv;
+
+    gestureZone.addEventListener('touchstart', function(event) {
+      touchstartX = event.changedTouches[0].screenX;
+      touchstartY = event.changedTouches[0].screenY;
+    }, false);
+
+    gestureZone.addEventListener('touchend', function(event) {
+      touchendX = event.changedTouches[0].screenX;
+      touchendY = event.changedTouches[0].screenY;
+      handleGesture();
+    }, false);
+
+    function handleGesture() {
+      if (touchendX < touchstartX) {
+        console.log('Swiped left');
+        handlePreviousArrow();
+      }
+
+      if (touchendX > touchstartX) {
+        console.log('Swiped right');
+        handleNextArrow();
+      }
+    }
   }
 
   slideShow();
